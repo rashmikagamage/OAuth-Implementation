a4fa6cb79a62e66be4db9f39c1af468da16d91f7
rashmika-nable <it18067866@my.sliit.lk>
2021-09-08 18:46:45 +0530

    completed git ednpoints

--- frontend/src/components/git-home.js
+++ frontend/src/components/git-home.js
@@ -1,65 +1,68 @@
-import "../css/twitter.css";
+import "../css/git.css";
+import code from "../images/code.png";
 import React, { useState } from "react";
 const axios = require("axios").default;
 
 function GitHome(props) {
   function getRepos() {}
   return (
-    <div>
+    <div class="main">
+       <img src={code} alt="Avatar" class="img"></img>
       <form>
-        <div class="form-group row">
+        <div class="form-group row mt-5">
           <input
             type="text"
             class="form-control"
             id="formGroupExampleInput"
-            placeholder="Example input"
+            placeholder="Repository Name"
           ></input>
           <input
             type="text"
-            class="form-control"
+            class="form-control mt-3"
             id="formGroupExampleInput"
-            placeholder="Example input"
+            placeholder="Description"
           ></input>
         </div>
-
         <table>
-  <tr>
-    <td>
-    <div class="form-check">
-          <input
-            class="form-check-input position-static"
-            type="radio"
-            name="blankRadio"
-            id="blankRadio1"
-            value="option1"
-            aria-label="..."
-          ></input>
-        </div>
-    </td>
-    <td>
-    <div class="form-check">
-          <input
-            class="form-check-input position-static"
-            type="radio"
-            name="blankRadio"
-            id="blankRadio1"
-            value="option1"
-            aria-label="..."
-          ></input>
-        </div>
-    </td>
-  </tr>
-  </table>
-
-        <div class = "row">
-          <div class="col-6">
-       
-        </div>
-        <div class = "col-6">
-       
-        </div>
-        </div>
+          <tr>
+            <td>
+              <div class="form-check">
+                <input
+                  class="form-check-input position-static"
+                  type="radio"
+                  name="public"
+                  id="true"
+                  value="option1"
+                  aria-label="..."
+                ></input>
+              </div>
+            </td>
+            <td>
+              <label class="form-check-label ml-1 mr-3" for="exampleRadios2">
+                Public
+              </label>
+            </td>
+            <td>
+              <div class="form-check">
+                <input
+                  class="form-check-input position-static"
+                  type="radio"
+                  name="private"
+                  id="false"
+                  value="false"
+                  aria-label="..."
+                ></input>
+              </div>
+            </td>
+            <td>
+              <label class="form-check-label ml-1 mr-3" for="exampleRadios2">
+                Private
+              </label>
+            </td>
+          </tr>
+        </table>
       </form>
+      <button type="button" class="btn btn-dark btn-sm btn-block mt-5">Create Repository</button>
     </div>
   );
 }
