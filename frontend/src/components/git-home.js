f56d4cd208672de722954e01abf0a71eb7e2ea9e
rashmika-nable <it18067866@my.sliit.lk>
2021-09-08 17:34:37 +0530

    created git login

--- frontend/src/components/git-home.js
+++ frontend/src/components/git-home.js
@@ -3,26 +3,63 @@ import React, { useState } from "react";
 const axios = require("axios").default;
 
 function GitHome(props) {
-
-    function getRepos(){
-        
-    }
+  function getRepos() {}
   return (
     <div>
-        <form>
-  <div class="form-group row">
-    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-    <div class="col-sm-10">
-      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com"></input>
-    </div>
-  </div>
-  <div class="form-group row">
-    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword" placeholder="Password"></input>
-    </div>
-  </div>
-</form>
+      <form>
+        <div class="form-group row">
+          <input
+            type="text"
+            class="form-control"
+            id="formGroupExampleInput"
+            placeholder="Example input"
+          ></input>
+          <input
+            type="text"
+            class="form-control"
+            id="formGroupExampleInput"
+            placeholder="Example input"
+          ></input>
+        </div>
+
+        <table>
+  <tr>
+    <td>
+    <div class="form-check">
+          <input
+            class="form-check-input position-static"
+            type="radio"
+            name="blankRadio"
+            id="blankRadio1"
+            value="option1"
+            aria-label="..."
+          ></input>
+        </div>
+    </td>
+    <td>
+    <div class="form-check">
+          <input
+            class="form-check-input position-static"
+            type="radio"
+            name="blankRadio"
+            id="blankRadio1"
+            value="option1"
+            aria-label="..."
+          ></input>
+        </div>
+    </td>
+  </tr>
+  </table>
+
+        <div class = "row">
+          <div class="col-6">
+       
+        </div>
+        <div class = "col-6">
+       
+        </div>
+        </div>
+      </form>
     </div>
   );
 }
